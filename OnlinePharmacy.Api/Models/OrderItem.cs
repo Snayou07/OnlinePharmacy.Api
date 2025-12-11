25452fbc1e42b5ac6302287e6f83b1ae62860982
Snayou07 <148194887+Snayou07@users.noreply.github.com>
2025-12-11 17:16:43 +0200

    Update OrderItem.cs

--- OnlinePharmacy.Api/Models/OrderItem.cs
+++ OnlinePharmacy.Api/Models/OrderItem.cs
@@ -6,16 +6,16 @@ namespace OnlinePharmacy.Api.Models
     {
         public long Id { get; set; }
 
-        // Связь с заказом
+     
         public long OrderId { get; set; }
         [JsonIgnore]
         public Order? Order { get; set; }
 
-        // Связь с лекарством
+        
         public long MedicineId { get; set; }
         public Medicine? Medicine { get; set; }
 
-        public int Quantity { get; set; } // Сколько штук купили
-        public decimal UnitPrice { get; set; } // Цена за штуку на момент покупки
+        public int Quantity { get; set; } 
+        public decimal UnitPrice { get; set; } 
     }
 }
