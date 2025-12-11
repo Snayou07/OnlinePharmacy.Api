63ad42064ae76452809578a59b2f799fb77bf12f
Snayou07 <148194887+Snayou07@users.noreply.github.com>
2025-12-11 17:15:51 +0200

    Update Category.cs

--- OnlinePharmacy.Api/Models/Category.cs
+++ OnlinePharmacy.Api/Models/Category.cs
@@ -8,7 +8,7 @@ namespace OnlinePharmacy.Api.Models
         public long Id { get; set; }
         public string Name { get; set; } = string.Empty;
 
-        // Связь: В категории много лекарств. JsonIgnore чтобы не было циклов при выводе.
+        
         [JsonIgnore]
         public List<Medicine> Medicines { get; set; } = new List<Medicine>();
     }
